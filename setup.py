eff9deae450d43227a222020aed9f6214e425c05
ThomLe <thomle@macs-MacBook-Pro.local>
2021-12-09 17:40:04 +0700

    Update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
+import pathlib
 from setuptools import find_packages, setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name='glaring_simulation',
     packages=find_packages(include=['glaring_simulation']),
     version='0.1.0',
-    description='Augmentation methods: glaring',
+    description='Augmentation methods: glaring
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thomle295/glaring_simulation",
     author='ThomLe',
+    author_email="thomlestudy295@gmail.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
     license='MIT',
     install_requires=[
         'numpy',
